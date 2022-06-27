acb80c65bde3297d9efe9ddf8a2aab9f0337925e
bigbagabum <78903783+bigbagabum@users.noreply.github.com>
2022-06-27 12:27:15 +0300

    res, roboto, metrics...

--- places/places/lib/main.dart
+++ places/places/lib/main.dart
@@ -4,6 +4,20 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyFirstWidget extends StatelessWidget {
+  const MyFirstWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
+    return Container(
+      child: const Center(
+        child: Text('Hello!'),
+      ),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
