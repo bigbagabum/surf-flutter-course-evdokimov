353b4faeac548e95086fb0989bce374b4589e0f4
bigbagabum <78903783+bigbagabum@users.noreply.github.com>
2022-07-08 17:20:53 +0300

    1

---
+++ app_new/flutter_application/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\Flutter\\flutter_windows_3.0.2-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\Flutter\\github\\flutter-course\\surf-flutter-course-evdokimov\\app_new\\flutter_application" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\Flutter\\flutter_windows_3.0.2-stable\\flutter"
+  "PROJECT_DIR=E:\\Flutter\\github\\flutter-course\\surf-flutter-course-evdokimov\\app_new\\flutter_application"
+  "FLUTTER_ROOT=E:\\Flutter\\flutter_windows_3.0.2-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\Flutter\\github\\flutter-course\\surf-flutter-course-evdokimov\\app_new\\flutter_application\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\Flutter\\github\\flutter-course\\surf-flutter-course-evdokimov\\app_new\\flutter_application"
+  "FLUTTER_TARGET=E:\\Flutter\\github\\flutter-course\\surf-flutter-course-evdokimov\\app_new\\flutter_application\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\Flutter\\github\\flutter-course\\surf-flutter-course-evdokimov\\app_new\\flutter_application\\.dart_tool\\package_config.json"
+)
