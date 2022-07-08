42835db02296b1b67054452251695661a21a1f76
bigbagabum <78903783+bigbagabum@users.noreply.github.com>
2022-07-08 18:12:44 +0300

    2

--- places/places/lib/ui/screen/sight_list_screen.dart
+++ places/places/lib/ui/screen/sight_list_screen.dart
@@ -13,6 +13,15 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        //elevation: 0,
+
+        backgroundColor: Colors.white10,
+        title: Text("Список\nинтересных местов",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+                fontSize: 32, color: Color.fromARGB(255, 214, 38, 38))),
+      ),
       body: Center(
           child: TextField(
               decoration: InputDecoration(
