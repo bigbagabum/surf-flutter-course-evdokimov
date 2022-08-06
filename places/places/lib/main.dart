1eebcb8f398341dc831672180afea71eff7f5e72
bigbagabum <78903783+bigbagabum@users.noreply.github.com>
2022-08-06 21:59:58 +0300

    иконка

--- places/places/lib/main.dart
+++ places/places/lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:places/ui/screen/sight_card.dart';
 import 'package:places/ui/screen/sight_details.dart';
 import 'package:places/ui/screen/sight_list_screen.dart';
-
-//import 'package:places/ui/screen/sight_card.dart';
+import 'package:places/mocks.dart';
+import 'package:places/ui/screen/sight_card.dart';
 
 void main() {
   runApp(const MyApp());
