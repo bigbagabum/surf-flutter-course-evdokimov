444928b6dc46136616c5a53275d1b586d013f852
bigbagabum <78903783+bigbagabum@users.noreply.github.com>
2022-10-10 15:55:02 +0300

    4 items in bottom nav bar

---
+++ places/places/lib/ui/screen/map_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
